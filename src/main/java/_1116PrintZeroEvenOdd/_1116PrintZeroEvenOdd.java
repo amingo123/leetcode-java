a9295d349280ed2750d64e18e664076902c8b115
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-01-07 11:40:37 +0800

    1116

--- src/main/java/_1116PrintZeroEvenOdd/_1116PrintZeroEvenOdd.java
+++ src/main/java/_1116PrintZeroEvenOdd/_1116PrintZeroEvenOdd.java
@@ -1,20 +1,51 @@
 package _1116PrintZeroEvenOdd;
 
+import java.util.concurrent.Semaphore;
+
 public class _1116PrintZeroEvenOdd {
     public static void main(String[] args) {
+        ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(5);
+        IntConsumer intConsumer = new IntConsumer();
         new Thread(()->
         {
             try {
-                fooBar.bar(() -> System.out.print("bar"));
+                zeroEvenOdd.zero(intConsumer);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        },"foo").start();
+        },"zero").start();
+        new Thread(()->
+        {
+            try {
+                zeroEvenOdd.even(intConsumer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"even").start();
+        new Thread(()->
+        {
+            try {
+                zeroEvenOdd.odd(intConsumer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"odd").start();
+    }
+}
+
+class IntConsumer
+{
+    void accept(int x)
+    {
+        System.out.print(x);
     }
 }
 
 class ZeroEvenOdd {
     private int n;
+    Semaphore zero = new Semaphore(1);
+    Semaphore even = new Semaphore(0);
+    Semaphore odd = new Semaphore(0);
 
     public ZeroEvenOdd(int n) {
         this.n = n;
@@ -22,14 +53,30 @@ class ZeroEvenOdd {
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-
+        for (int i = 1; i<=n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                even.release();
+            } else {
+                odd.release();
+            }
+        }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
-
+        for (int i = 2; i<=n; i=i+2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-
+        for (int i = 1; i<=n; i=i+2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
     }
 }

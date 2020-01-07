12c0dce1add03246dffd445002dc2833dae9631c
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-01-07 09:08:02 +0800

    1116

---
+++ src/main/java/_1116PrintZeroEvenOdd/_1116PrintZeroEvenOdd.java
@@ -0,0 +1,35 @@
+package _1116PrintZeroEvenOdd;
+
+public class _1116PrintZeroEvenOdd {
+    public static void main(String[] args) {
+        new Thread(()->
+        {
+            try {
+                fooBar.bar(() -> System.out.print("bar"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"foo").start();
+    }
+}
+
+class ZeroEvenOdd {
+    private int n;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
